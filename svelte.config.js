0c660a2a18609ac27ddebe5454005a4a5d7993b5
SICM-STEVE <you@example.comsteven.roberts@sicm.com>
2023-02-27 16:11:58 +0000

    app service

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import azure from 'svelte-adapter-azure-swa';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: azure(),
+		adapter: adapter(),
 		alias: {
 			$lib: './src/lib'
 		}		
