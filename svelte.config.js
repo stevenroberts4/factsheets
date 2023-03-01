2a377cf7698e2d06e9f659de7d9a030baa9868ab
SICM-STEVE <you@example.comsteven.roberts@sicm.com>
2023-03-01 14:47:49 +0000

    back to azure swa adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,65 @@
-import adapter from '@sveltejs/adapter-static';
+import azure from 'svelte-adapter-azure-swa';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: null
+		adapter: azure({
+			customStaticWebAppConfig: {
+				"auth": {
+				  "identityProviders": {
+					"azureActiveDirectory": {
+					  "registration": {
+						"openIdIssuer": "https://login.microsoftonline.com/9f9dd56c-1ee0-4a26-bf64-e6039151b4f8/v2.0",
+						"clientIdSettingName": "AZURE_CLIENT_ID",
+						"clientSecretSettingName": "AZURE_CLIENT_SECRET"
+					  }
+					}
+				  }
+				},
+				/*
+				"navigationFallback": {
+				  "rewrite": "index.html"
+				},
+				*/
+				"routes": [
+				  {
+					"route": "/login",
+					"rewrite": "/.auth/login/aad",
+					"allowedRoles": ["anonymous", "authenticated"]
+				  },
+				  {
+					"route": "/.auth/login/github",
+					"statusCode": 404
+				  },
+				  {
+					"route": "/.auth/login/twitter",
+					"statusCode": 404
+				  },
+				  {
+					"route": "/logout",
+					"redirect": "/.auth/logout",
+					"allowedRoles": ["anonymous", "authenticated"]
+				  },
+				  {
+					"route": "/*",
+					"allowedRoles": ["authenticated"]
+				  }
+				],
+				"responseOverrides": {
+				  "401": {
+					"redirect": "/login",
+					"statusCode": 302
+				  }
+				},
+				"globalHeaders": {
+				  "content-security-policy": "default-src https: 'unsafe-eval' 'unsafe-inline'; object-src 'none'"
+				},
+				"mimeTypes": {
+				  ".json": "text/json"
+				}
+			  }
 		}),
 		alias: {
 			$lib: './src/lib'
@@ -19,55 +68,3 @@ const config = {
 };
 
 export default config;
-
-
-/*
-adapter: azure({
-			customStaticWebAppConfig: {
-				"auth": {
-					"identityProviders": {
-					  "azureActiveDirectory": {
-						"registration": {
-						  "openIdIssuer": "https://login.microsoftonline.com/9f9dd56c-1ee0-4a26-bf64-e6039151b4f8/v2.0",
-						  "clientIdSettingName": "AZURE_CLIENT_ID",
-						  "clientSecretSettingName": "AZURE_CLIENT_SECRET"
-						}
-					  }
-					}
-				  },
-				  "routes": [
-					{
-					  "route": "/login",
-					  "rewrite": "/.auth/login/aad",
-					  "allowedRoles": ["anonymous", "authenticated"]
-					},
-					{
-					  "route": "/.auth/login/github",
-					  "statusCode": 404
-					},
-					{
-					  "route": "/.auth/login/twitter",
-					  "statusCode": 404
-					},
-					{
-					  "route": "/logout",
-					  "redirect": "/.auth/logout",
-					  "allowedRoles": ["anonymous", "authenticated"]
-					}
-				  ],
-				  "responseOverrides": {
-					"401": {
-					  "redirect": "/login",
-					  "statusCode": 302
-					}
-				  },
-				  "globalHeaders": {
-					"content-security-policy": "default-src https: 'unsafe-eval' 'unsafe-inline'; object-src 'none'"
-				  },
-				  "mimeTypes": {
-					".json": "text/json"
-				  }
-			}
-		}),
-
-		*/
