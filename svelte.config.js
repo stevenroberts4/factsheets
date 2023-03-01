3c4ce08afb67a6ee999a1eddffa50c4453250053
SICM-STEVE <you@example.comsteven.roberts@sicm.com>
2023-03-01 14:39:49 +0000

    attempting static adaptor

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,28 @@
-import azure from 'svelte-adapter-azure-swa';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: azure({
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
+		alias: {
+			$lib: './src/lib'
+		}		
+	}
+};
+
+export default config;
+
+
+/*
+adapter: azure({
 			customStaticWebAppConfig: {
 				"auth": {
 					"identityProviders": {
@@ -52,10 +69,5 @@ const config = {
 				  }
 			}
 		}),
-		alias: {
-			$lib: './src/lib'
-		}		
-	}
-};
 
-export default config;
+		*/
